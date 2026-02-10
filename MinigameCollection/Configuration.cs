8adfef760bc5baf44d4fefc6d83e2c9e16168218
Pistachio-dev <177745391+Pistachio-dev@users.noreply.github.com>
2026-02-10 19:06:14 +0100

    Added configuration and chat channel select

--- MinigameCollection/Configuration.cs
+++ MinigameCollection/Configuration.cs
@@ -14,8 +14,6 @@ public class Configuration : IConfiguration
     public bool LogClientOnlyChatOutput { get; set; } = true;
     public int LimitedChatChannelsMessageDelayInMs { get; set; } = 1000;
 
-    public bool UsingRealGil { get; set; } = true;
-
     public int SelectedGame { get; set; } = 0;
 
     public AcceptedRollType AcceptedRollType { get; set; } = AcceptedRollType.Any;
--- MinigameCollection/UI/Windows/ConfigWindow.cs
+++ MinigameCollection/UI/Windows/ConfigWindow.cs
@@ -1,23 +1,28 @@
 using Dalamud.Bindings.ImGui;
+using Dalamud.Game.Text;
 using DalamudBasics.Configuration;
+using DalamudBasics.GUI.Forms;
 using DalamudBasics.GUI.Windows;
 using DalamudBasics.Logging;
+using ECommons.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 using System.Numerics;
 
 namespace MinigameCollection.UI.Windows;
 
 public class ConfigWindow : PluginWindowBase, IDisposable
 {
-    private IConfiguration configuration;
+    private IConfigurationService<Configuration> configService;
+    private Configuration configuration;
+    private ImGuiFormFactory<Configuration> formFactory;
 
-    public ConfigWindow(ILogService logService, IServiceProvider sp) : base(logService, "Configuration")
+    public ConfigWindow(ILogService logService, IServiceProvider sp) : base(logService, "Configuration##MinigameCollectionConfiguration")
     {
-        Size = new Vector2(232, 90);
-        SizeCondition = ImGuiCond.Always;
-
-        configuration = sp.GetRequiredService<IConfiguration>();
+        configService = sp.GetRequiredService<IConfigurationService<Configuration>>();
+        configuration = configService.GetConfiguration();
+        formFactory = new ImGuiFormFactory<Configuration>(() => configuration, (data) => Save());
     }
 
     public void Dispose()
@@ -29,5 +34,36 @@ public class ConfigWindow : PluginWindowBase, IDisposable
 
     protected override void SafeDraw()
     {
+        DrawChatChannelSelector();
     }
+
+    private void DrawChatChannelSelector()
+    {
+        XivChatType[] channels = { XivChatType.Echo, XivChatType.Party, XivChatType.Alliance, XivChatType.Say,
+                XivChatType.Ls1, XivChatType.Ls2, XivChatType.Ls3, XivChatType.Ls4, XivChatType.Ls5, XivChatType.Ls6, XivChatType.Ls7, XivChatType.Ls8,
+                XivChatType.CrossLinkShell1, XivChatType.CrossLinkShell2, XivChatType.CrossLinkShell3, XivChatType.CrossLinkShell4, XivChatType.CrossLinkShell5, XivChatType.CrossLinkShell6, XivChatType.CrossLinkShell7, XivChatType.CrossLinkShell8};
+        string[] options = channels.Select(entry => entry.ToString()).ToArray();
+        var local = channels.IndexOf(configService.GetConfiguration().DefaultOutputChatType);
+        if (local == -1)
+        {
+            local = 0;
+        }
+        if (ImGui.Combo("Chat channel", ref local, options))
+        {
+            if (local >= channels.Length)
+            {
+                return;
+            }
+            configService.GetConfiguration().DefaultOutputChatType = channels[local];
+            configService.SaveConfiguration();
+        }
+    }
+
+    private void Save()
+    {
+        configService.SaveConfiguration();
+        logService.Info("Configuration saved.");
+    }
+
+
 }
